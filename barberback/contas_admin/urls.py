971400988c2279dcf70e54770907cffb018bf790
abraao <abraao@kali-abraao.dev>
2026-01-30 22:51:14 -0300

    Template para login e logout atualizados com bootstrap5.

--- barberback/contas_admin/urls.py
+++ barberback/contas_admin/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-
-from . import views
+from django.contrib.auth import views
 
 app_name = "contas_admin"
 urlpatterns = [
     path('', 
-         views.LoginAdminView.as_view(), name="login"),
+         views.LoginView.as_view(template_name="contas_admin/login.html"), name="login"),
     path('logout/', 
-         views.LogoutAdminView.as_view(), name="logout"),
+         views.LogoutView.as_view(template_name="contas_admin/logged_out.html"), name="logout"),
 ]
